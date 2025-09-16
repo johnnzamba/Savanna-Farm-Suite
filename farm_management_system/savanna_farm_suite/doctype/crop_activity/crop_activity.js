8846f8eb1c8155ce14a1659d3649d814a8bfea5f
Minstr <nzambakitheka@gmail.com>
2025-09-16 16:49:28 +0300

    Crops

---
+++ farm_management_system/savanna_farm_suite/doctype/crop_activity/crop_activity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Techsavanna Technology and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Crop Activity", {
+// 	refresh(frm) {
+
+// 	},
+// });
